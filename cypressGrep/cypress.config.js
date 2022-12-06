9866c483a5063c30239036739bab1f99e61f58ef
Modern Sapien <joncanales2@gmail.com>
2022-12-05 20:16:08 -0500

    video false, screenshot false.

--- cypressGrep/cypress.config.js
+++ cypressGrep/cypress.config.js
@@ -5,8 +5,9 @@ module.exports = defineConfig({
     baseUrl: "https://jsonplaceholder.typicode.com/",
     // use Studio to generate tests fast by UI!
     experimentalStudio: true,
+    video: false,
+    screenshotOnRunFailure: false,
 
-    
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require("@cypress/grep/src/plugin")(config);
